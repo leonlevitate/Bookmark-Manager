244e16b65c22b27981d472e14eaea9fe0fc95ffb
leon.linton@live.co.uk <leon.linton@live.co.uk>
2019-06-17 18:55:22 +0100

    Created Bookmark Model and View, updated Controller

---
+++ lib/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark
+  def self.all
+    [
+      "http://www.bbc.com",
+      "http://www.netflix.com",
+      "http://www.youtube.com"
+     ]
+  end
+end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,6 +1,9 @@
 feature 'Viewing bookmarks' do
-  scenario 'visiting the index page' do
-    visit('/')
-    expect(page).to have_content "Bookmark Manager"
+  scenario 'returns all bookmarks' do
+    visit('/bookmarks')
+    # click_button('View all Bookmarks')
+    expect(page).to have_content "http://www.bbc.com"
+    expect(page).to have_content "http://www.netflix.com"
+    expect(page).to have_content "http://www.youtube.com"
   end
 end
